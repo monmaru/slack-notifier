5cefa92de0d73329e61491edf7f2334c81a37254
Yoshitomo Minami <minami530@gmail.com>
2016-09-24 16:45:39 +0900

    Added type hints

--- message.py
+++ message.py
@@ -3,8 +3,11 @@ import os
 from contextlib import contextmanager
 from datetime import datetime
 from glob import glob
+from typing import Dict, List
+
 import matplotlib.pyplot as plt
-import pandas as pd
+from pandas import DataFrame
+
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -18,7 +21,7 @@ class MessageBuilder(object):
     FW_URL = 'https://t-analyzers.github.io/featurewords'
     IL_URL = 'https://t-analyzers.github.io/imagelist'
 
-    def __init__(self, data_dir):
+    def __init__(self, data_dir: str):
         self._data_dir = data_dir
         self._tc_message = ''
         self._fw_message = ''
@@ -26,7 +29,7 @@ class MessageBuilder(object):
         self._word_cloud_path = ''
 
     @contextmanager
-    def build(self, target_date: datetime):
+    def build(self, target_date: datetime) -> None:
         """
         通知するメッセージとグラフを生成する。
         各プロパティにアクセスする前にこの関数を呼び出す必要がある。
@@ -50,7 +53,7 @@ class MessageBuilder(object):
             if os.path.exists(self._chart_path):
                 os.remove(self._chart_path)
 
-    def _build_tweet_count_message(self, data_list):
+    def _build_tweet_count_message(self, data_list: List[Dict]) -> str:
         """
         つぶやき数をサマライズしたメッセージを生成する
         """
@@ -71,7 +74,6 @@ class MessageBuilder(object):
         before_rt_count = before_data['retweets_count']
         before_t_count = before_total - before_rt_count
 
-        # つぶやき数のメッセージ作成
         return self.LF.join([
             '合計：  {0}{1}'.format(str(total), build_difference(before_total, total)),
             'リツィート除く：  {0}{1}'.format(str(t_count), build_difference(before_t_count, t_count)),
@@ -79,7 +81,7 @@ class MessageBuilder(object):
         ])
 
     @classmethod
-    def _build_feature_words_message(cls, data_list):
+    def _build_feature_words_message(cls, data_list: List[Dict]) -> str:
         """
         特徴語ランキングのメッセージを生成する
         """
@@ -87,14 +89,13 @@ class MessageBuilder(object):
         return cls.LF.join(['{0}位  {1}'.format(str(i + 1), v) for i, v in enumerate(latest_data['feature_words'])])
 
     @classmethod
-    def _create_chart_img(cls, data_list):
+    def _create_chart_img(cls, data_list: List[Dict]) -> str:
         """
         つぶやき数の推移グラフを生成する
         """
         # noinspection PyUnresolvedReferences
         import seaborn as sns
-        temp_img_path = os.path.join(os.path.dirname(__file__), 'temp/graph.png')
-        df = pd.DataFrame(
+        df = DataFrame(
             index=[data['date'] for data in data_list],
             data={
                 '#ALL': [data['tweets_count'] for data in data_list],
@@ -108,11 +109,12 @@ class MessageBuilder(object):
         plt.title('Weekly')
         plt.xlabel('date')
         plt.ylabel('tweet count')
+        temp_img_path = os.path.join(os.path.dirname(__file__), 'temp/graph.png')
         plt.savefig(temp_img_path)
         return temp_img_path
 
     @classmethod
-    def _build_attachments(cls, title: str, text: str):
+    def _build_attachments(cls, title: str, text: str) -> List[Dict]:
         """
         Attachmentsを生成する
         https://api.slack.com/docs/message-attachments
@@ -125,21 +127,21 @@ class MessageBuilder(object):
         }]
 
     @property
-    def word_cloud_path(self):
+    def word_cloud_path(self) -> str:
         return self._word_cloud_path
 
     @property
-    def chart_path(self):
+    def chart_path(self) -> str:
         return self._chart_path
 
     @property
-    def tweet_count_attachments(self):
+    def tweet_count_attachments(self) -> List[Dict]:
         return self._build_attachments('つぶやき数', self._tc_message)
 
     @property
-    def feature_words_attachments(self):
+    def feature_words_attachments(self) -> List[Dict]:
         return self._build_attachments('特徴語ランキング', self._fw_message)
 
     @property
-    def more_info(self):
+    def more_info(self) -> str:
         return self.LF.join(['more info...', self.FW_URL, self.IL_URL])
--- slack.py
+++ slack.py
@@ -12,7 +12,6 @@ from message import MessageBuilder
 
 
 class SlackBot(object):
-
     BOT_NAME = 't-analyzers-bot'
     ICON_EMOJI = ':ghost:'
 
@@ -20,22 +19,21 @@ class SlackBot(object):
         setting_file = os.path.join(os.path.dirname(__file__), 'setting.yml')
         with open(setting_file, 'r', encoding='utf-8') as file:
             setting = yaml.load(file)
-            self.token = setting['slack']['token'],
+            self.slack = Slacker(setting['slack']['token'])  # type: Slacker
             self.channel = setting['slack']['channel']
             self.data_dir = setting['data']['dir']
 
-    async def notify(self):
+    async def notify(self) -> None:
         """
         Slackに解析結果を通知する
         """
-        slack = Slacker(self.token)
         yesterday = datetime.now() - timedelta(days=1)
         common_text = '■{0}の解析結果'.format(yesterday.strftime('%Y/%m/%d'))
 
         message = MessageBuilder(self.data_dir)
         with message.build(yesterday):
             # つぶやき数
-            slack.chat.post_message(
+            self.slack.chat.post_message(
                 channel=self.channel,
                 text=common_text,
                 attachments=message.tweet_count_attachments,
@@ -44,14 +42,14 @@ class SlackBot(object):
             )
 
             # つぶやき数のグラフ
-            slack.files.upload(
+            self.slack.files.upload(
                 message.chart_path,
                 title='つぶやき数の推移',
                 channels=self.channel
             )
 
             # 特徴語ランキング
-            slack.chat.post_message(
+            self.slack.chat.post_message(
                 channel=self.channel,
                 text=common_text,
                 attachments=message.feature_words_attachments,
@@ -60,14 +58,14 @@ class SlackBot(object):
             )
 
             # Word Cloud
-            slack.files.upload(
+            self.slack.files.upload(
                 message.word_cloud_path,
                 title='Word Cloud',
                 channels=self.channel
             )
 
             # Webサイトへのリンク
-            slack.chat.post_message(
+            self.slack.chat.post_message(
                 channel=self.channel,
                 text=message.more_info,
                 username=self.BOT_NAME,
@@ -78,7 +76,7 @@ class SlackBot(object):
             await asyncio.sleep(5)
 
 
-def run():
+def run() -> None:
     # noinspection PyBroadException
     try:
         loop = asyncio.get_event_loop()
@@ -86,6 +84,7 @@ def run():
     except:
         import traceback
         traceback.print_exc()
+
 
 if __name__ == '__main__':
     run()
